30cae0c40d0f3ebf07cccb755fd6daefb515a8d0
Smriti <smritisharma7113@gmail.com>
2025-05-07 21:31:43 -0500

    Story_4 - Created Footer
    Added code for Footer and added the component  to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
     <Router>
       <div className="App">
         <Header />
+        <Footer />
       </div>
     </Router>
   );
