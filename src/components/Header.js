550277d3529edfaa7c1fad508b2cd7aa313e46ca
Smriti <smritisharma7113@gmail.com>
2025-05-07 21:28:51 -0500

    Story_3 - Created Header
    Created Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>Delicious Meals</h1>
+      </div>
+      <p>Your favorite meals throughout the day</p>
+    </header>
+  );
+};
+
+export default Header;
